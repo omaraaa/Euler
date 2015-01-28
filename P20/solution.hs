c5de1dc580ac9944b59904df48658a342e89f165
Omar Alhammadi <SequenceKO@gmail.com>
2015-01-28 16:37:17 +0400

    Problem 20 in Haskell

---
+++ P20/solution.hs
@@ -0,0 +1,15 @@
+module Main where
+
+fact x = f x 1
+	where f num count
+		| num == 1 = count
+		| otherwise = f (num-1) (count*num)
+
+
+
+seprateDigits n l
+	| n == 0 = l
+	| otherwise = seprateDigits (n `div` 10) (l ++ [n `mod` 10])
+
+main :: IO()
+main = print $ sum (seprateDigits (fact 100) [])
